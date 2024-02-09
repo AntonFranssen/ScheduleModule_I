6dc20bddf674522d569d9f24dfe70410b556bb2c
AntonFranssen <145449774+AntonFranssen@users.noreply.github.com>
2024-02-09 21:28:24 +0300

    Update server_exceptions.cpp

--- server_exceptions.cpp
+++ server_exceptions.cpp
@@ -4,7 +4,7 @@ http::response<http::string_body> Exception::what() {
 	http::response<http::string_body> response{
 		st, version
 	};
-	response.set(http::field::server, "RedArmy.il");
+	response.set(http::field::server, "ScheduleServer.com");
 	response.set(http::field::content_type, "text/html");
 	response.keep_alive(this->is_keep_alive);
 	response.body() = this->error;
